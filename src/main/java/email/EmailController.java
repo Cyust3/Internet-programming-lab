51a1479d23836e59caa5b5a3b25d1f39e7b8dfa6
Андрей Бердяев <65670124+Cyust3@users.noreply.github.com>
2025-05-16 09:45:32 +0400

    Lab
    Проект

---
+++ src/main/java/email/EmailController.java
@@ -0,0 +1,42 @@
+package email;
+
+import email.model.Email;
+import email.model.EmailForm;
+import email.service.EmailService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EmailController {
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/")
+    public String indexForm(Model model) {
+        model.addAttribute("emailForm", new EmailForm());
+        return "index";
+    }
+
+    @PostMapping("/sendEmail")
+    public String sendEmail(@ModelAttribute EmailForm emailForm, Model model) {
+        if (emailForm.getTo().isEmpty()) {
+            model.addAttribute("error", "Поле 'Ваш e-mail для обратной связи' не должно быть пустым");
+            return "index";
+        }
+        emailService.save(new Email(emailForm.getTo(), emailForm.getSubject(), emailForm.getMessage()));
+        model.addAttribute("emails", emailService.getAllEmails());
+        return "list";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("emails", emailService.getAllEmails());
+        return "list";
+    }
+}
